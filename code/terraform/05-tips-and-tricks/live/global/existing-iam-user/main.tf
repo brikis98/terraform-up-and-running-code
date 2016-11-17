2494a715f524c1155b23082959712ff7791fbbc8
Yevgeniy Brikman <brikis98@gmail.com>
2016-11-17 17:28:33 +0000

    Update comment in IAM code

--- code/terraform/05-tips-and-tricks/live/global/existing-iam-user/main.tf
+++ code/terraform/05-tips-and-tricks/live/global/existing-iam-user/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_iam_user" "existing_user" {
-  # TODO: you should change this to the username of an IAM user that already
+  # You should change this to the username of an IAM user that already
   # exists so you can practice using the terraform import command
   name = "yevgeniy.brikman"
 }
