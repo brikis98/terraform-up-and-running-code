2b64b2e1cd7a3405af97642bcd7a83f4267d881c
Yevgeniy Brikman <brikis98@gmail.com>
2017-05-01 12:22:44 +0100

    Add skip_final_snapshot to all DB resources
    Fixes #15

--- code/terraform/05-tips-and-tricks/loops-and-if-statements/live/prod/data-stores/mysql/main.tf
+++ code/terraform/05-tips-and-tricks/loops-and-if-statements/live/prod/data-stores/mysql/main.tf
@@ -7,10 +7,11 @@ provider "aws" {
 }
 
 resource "aws_db_instance" "example" {
-  engine            = "mysql"
-  allocated_storage = 10
-  instance_class    = "db.t2.micro"
-  name              = "example_database_prod"
-  username          = "admin"
-  password          = "${var.db_password}"
+  engine              = "mysql"
+  allocated_storage   = 10
+  instance_class      = "db.t2.micro"
+  name                = "example_database_prod"
+  username            = "admin"
+  password            = "${var.db_password}"
+  skip_final_snapshot = true
 }
