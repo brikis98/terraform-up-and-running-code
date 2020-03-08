196f675cd63722cec7ba03493e2f1c4b5eb15561
Dan Tracey <dtracey@hotmail.co.uk>
2020-03-08 17:09:47 +0000

    Update setup-webserver.sh

--- code/bash/01-why-terraform/setup-webserver.sh
+++ code/bash/01-why-terraform/setup-webserver.sh
@@ -6,8 +6,8 @@ set -e
 # Update the apt-get cache
 sudo apt-get update
 
-# Install PHP and Apache
-sudo apt-get install -y php apache2
+# Install PHP, Apache and git
+sudo apt-get install -y php apache2 git
 
 # Copy the code from the repository
 sudo git clone https://github.com/brikis98/php-app.git /var/www/html/app
