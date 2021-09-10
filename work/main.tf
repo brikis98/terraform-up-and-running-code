ce015b9a5b3e5254c989246ff9ec4e72b4b81b49
Alvaro del Castillo San Felix <alvaro.delcastillo@smartprotection.com>
2021-09-10 08:10:35 +0200

    Add public web server to the EC2 instance

--- work/main.tf
+++ work/main.tf
@@ -5,8 +5,26 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami = "ami-00622de605b25a7d6"  # Ubuntu 21.04 amd64
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+    #!/bin/bash
+    echo "Hello, World" > index.html
+    nohup busybox httpd -f -p 8080 &
+  EOF
 
   tags = {
     Name = "terraform-example"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port = 8080
+    protocol = "tcp"
+    to_port = 8080
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
