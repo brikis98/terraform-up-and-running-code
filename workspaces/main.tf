e554dd47468245df29196bb40aa5db11f7854a26
Alvaro del Castillo San Felix <alvaro.delcastillo@smartprotection.com>
2021-09-15 08:35:09 +0200

    Add sample with workspaces

--- workspaces/main.tf
+++ workspaces/main.tf
@@ -3,6 +3,17 @@ provider "aws" {
   max_retries = 1
 }
 
+terraform {
+  backend "s3" {
+    bucket = "terraform-up-and-running-acs-state"
+    key = "workspaces/terraform.tfstate"
+    region = "eu-west-1"
+
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt = true
+  }
+}
+
 resource "aws_instance" "example" {
   ami = var.ami_ubuntu_21_04
   instance_type = "t2.micro"
