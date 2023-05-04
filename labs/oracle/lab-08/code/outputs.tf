3d9c202693792dad8e5364496ce328fdc33a741b
kumarhimanshu449 <kumarhimanshu449@gmail.com>
2023-05-04 14:18:01 +0100

    updated labs

--- labs/oracle/lab-08/code/outputs.tf
+++ labs/oracle/lab-08/code/outputs.tf
@@ -1,10 +1,4 @@
-
-output "EC2_ami" {
-    description = "ami type used in myVM"
-    value = aws_instance.myVM.ami
-}
-
-output "EC2_type" {
+output "Instance_type" {
     description = "instance type used in myVM"
     value = "Hi, my name is ${local.name}"
 }
