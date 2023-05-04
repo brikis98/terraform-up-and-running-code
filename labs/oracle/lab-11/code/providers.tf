53b7d27caf18a5c866c07badb5b119e061f4be37
kumarhimanshu449 <kumarhimanshu449@gmail.com>
2023-05-04 18:44:49 +0100

    Update providers.tf

--- labs/oracle/lab-11/code/providers.tf
+++ labs/oracle/lab-11/code/providers.tf
@@ -1,17 +1,8 @@
 provider "oci" {
-  alias            = "home"
-  region           = "<region_1>"
-  tenancy_ocid     = "<region_1_tenancy_ocid>"
-  user_ocid        = "<region_1_user_ocid>"
-  fingerprint      = "<region_1_fingerprint>"
-  private_key_path = "<region_1_private_key_path>"
+  tenancy_ocid     = "ocid1.tenancy.oc1..aaaaaaaa6ag77lnluy7avrzhjkg7g3kriz5t5u2jnsw43yecx4oylzxsv5uq"
 }
 
 provider "oci" {
   alias            = "region2"
-  region           = "<region_2>"
-  tenancy_ocid     = "<region_2_tenancy_ocid>"
-  user_ocid        = "<region_2_user_ocid>"
-  fingerprint      = "<region_2_fingerprint>"
-  private_key_path = "<region_2_private_key_path>"
+  tenancy_ocid     = "ocid1.compartment.oc1..aaaaaaaayf3gksj2b37farc2tep72goofuc25tzsd72blhdjmel54kwv2xla"
 }
