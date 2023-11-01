8dd978f88c88a240afd85a34de15c04c0e4f76f6
Joachim Gourdon (RIC-CH) <joachim.gourdon@richemont.com>
2023-11-01 19:41:00 +0100

    feat: Add shell script to set the env variable for AWS

--- code/terraform/00-preface/hello-world/main.tf
+++ code/terraform/00-preface/hello-world/main.tf
@@ -16,5 +16,9 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-0fb653ca2d3203ac1"
   instance_type = "t2.micro"
+
+  tags = {
+    Name = "terraform-example"
+  }
 }
 
