dd9c7d240839433da085c88b5b45842d71eb0a23
Fredrik B. Hedman <fredrik.hedman@noruna.se>
2023-12-12 16:51:45 +0100

    Clarify what the key is used for

--- code/terraform/03-terraform-state/file-layout-example/stage/services/webserver-cluster/variables.tf
+++ code/terraform/03-terraform-state/file-layout-example/stage/services/webserver-cluster/variables.tf
@@ -10,7 +10,7 @@ variable "db_remote_state_bucket" {
 }
 
 variable "db_remote_state_key" {
-  description = "The name of the key in the S3 bucket used for the database's remote state storage"
+  description = "The name of the key in the S3 bucket used for the data stores database's remote state storage"
   type        = string
   default = "stage/data-stores/mysql/terraform.tfstate"
 }
